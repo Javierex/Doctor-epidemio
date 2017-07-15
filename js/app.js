92f70eff9aafbeb4197aa123a55fbf044044b098
Javier Perez <javierepeca@gmail.com>
2017-07-15 12:07:55 -0500

    plantilla del index

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$('#telefono').click(function mostrarModal (){
+	$('#modalTelefono').modal()
+})
+
+$('#correo').click(function mostrarModal (){
+	$('#modalCorreo').modal()
+})
+
+$('#arriba').click(function(e){
+	jQuery('html, body').animate({ scrollTop: 0 }, { duration: 300, easing: "swing" });
+	e.preventDefault();
+});
+
+$("inicio").hover(function () {
+    $(this).addClass('fa-cogs');
+  });
