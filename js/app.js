9a75feda26d0e69db4c4d5b4e13b2a9ae12f43ba
Javier Perez <javierepeca@gmail.com>
2017-07-16 21:29:32 -0500

    Creacion de la pagina de servicios

--- js/app.js
+++ js/app.js
@@ -14,3 +14,63 @@ $('#arriba').click(function(e){
 $("inicio").hover(function () {
     $(this).addClass('fa-cogs');
   });
+
+$("#excelMas").click(function() {
+	$(this).addClass('none');
+	$(".parrafo").removeClass('none');
+	$("#excelMenos").removeClass('none');
+});
+
+$("#excelMenos").click(function() {
+	$(this).addClass('none');
+	$(".parrafo").addClass('none');
+	$("#excelMas").removeClass('none');
+});
+
+$("#accesMas").click(function() {
+	$(this).addClass('none');
+	$(".parrafoAcces").removeClass('none');
+	$("#accesMenos").removeClass('none');
+});
+
+$("#accesMenos").click(function() {
+	$(this).addClass('none');
+	$(".parrafoAcces").addClass('none');
+	$("#accesMas").removeClass('none');
+});
+
+$("#insertMas").click(function() {
+	$(this).addClass('none');
+	$(".parrafoInsert").removeClass('none');
+	$("#insertMenos").removeClass('none');
+});
+
+$("#insertMenos").click(function() {
+	$(this).addClass('none');
+	$(".parrafoInsert").addClass('none');
+	$("#insertMas").removeClass('none');
+});
+
+$("#analisisMas").click(function() {
+	$(this).addClass('none');
+	$(".parrafoAnalisis").removeClass('none');
+	$("#analisisMenos").removeClass('none');
+});
+
+$("#analisisMenos").click(function() {
+	$(this).addClass('none');
+	$(".parrafoAnalisis").addClass('none');
+	$("#analisisMas").removeClass('none');
+});
+
+$("#plataformaMas").click(function() {
+	$(this).addClass('none');
+	$(".parrafoPlataforma").removeClass('none');
+	$("#plataformaMenos").removeClass('none');
+});
+
+$("#plataformaMenos").click(function() {
+	$(this).addClass('none');
+	$(".parrafoPlataforma").addClass('none');
+	$("#plataformaMas").removeClass('none');
+});
